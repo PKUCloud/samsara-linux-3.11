0f87cf04e2807cd4f7abf9698807ccd1dfc4f639
Arthur Chunqi Li <yzt356@gmail.com>
2013-12-13 13:41:41 +0800

    Add record_ctrl.c, a userspace record control program.
    Signed-off-by: Arthur Chunqi Li <yzt356@gmail.com>

---
+++ record_ctrl.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+#define KVMIO 0xAE
+#define KVM_ENABLE_RECORD         _IO(KVMIO, 0x09)
+#define KVM_DISABLE_RECORD        _IO(KVMIO, 0x0a)
+#define KVM_GET_API_VERSION       _IO(KVMIO,   0x00)
+
+int main(int argc, char **argv)
+{
+	int fd;
+	int record;
+	int ret;
+	long type;
+	fd = open("/dev/kvm", 0);
+	if (fd < 0) {
+		printf("Open /dev/kvm failed\n");
+		return -1;
+	}
+	if (argc < 2) {
+		record = 1;
+	} else {
+		if (strcmp(argv[1], "enable") == 0)
+			record = 1;
+		else
+			record = 0;
+	}
+	if (record) {
+		ret = ioctl(fd, KVM_ENABLE_RECORD, 0);
+		if (ret < 0)
+			printf("KVM_ENABLE_RECORD failed, errno = %d\n", errno);
+	} else {
+		ret = ioctl(fd, KVM_DISABLE_RECORD, 0);
+		if (ret < 0)
+			printf("KVM_DISABLE_RECORD failed, errno = %d\n", errno);
+	}
+	return 0;
+}
