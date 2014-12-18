a1aabe574e3d4d137bb8159ae39988dd6b836412
root <root@xps.(none)>
2014-12-18 08:23:31 -0800

    Add profile header file.
    Add include/linux/rr_profile.h.
    
    For more profiling codes, see branch origin/profile.
    
    Signed-off-by: Chunqi Li

---
+++ include/linux/rr_profile.h
@@ -0,0 +1,30 @@
+#ifndef __RR_PROFILE_H
+#define __RR_PROFILE_H
+
+
+//#define RR_PROFILE
+#define RR_AD_BIT_OPT
+
+struct vmexit_states {
+	uint64_t num;
+	uint64_t time;
+};
+
+struct vcpu_rr_states {
+	uint64_t vm_time, kvm_time;
+	uint64_t total_commit_time, page_commit_time;
+	uint64_t walk_mmu_time, set_dirty_bit_time, detect_conflict_time;
+	struct vmexit_states vmexit_states[60];
+	int exit_reason;
+};
+
+static inline uint64_t rr_rdtsc(void)
+{
+	unsigned int low, high;
+
+	asm volatile("rdtsc" : "=a" (low), "=d" (high));
+
+	return low | ((unsigned long long)high) << 32;
+}
+
+#endif
